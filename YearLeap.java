4ffdb4a990201034689292f1848d8d1c5f693bdf
Tejashri Bachkar <92704050+BachkarTeju@users.noreply.github.com>
2023-08-31 10:52:20 +0530

    Update YearLeap.java

--- YearLeap.java
+++ YearLeap.java
@@ -2,8 +2,6 @@ public class YearLeap {
   public static void main(String[] args) {
     int year = 2023;
     boolean isLeapYear;
-
-    // divisible by 4
     isLeapYear = (year % 4 == 0);
     isLeapYear = isLeapYear && (year % 100 != 0 || year % 400 == 0);
 
